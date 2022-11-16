23286b5f53a9d1e922e340430b412e16297250d8
Javier <santiagodiaz0201@gmail.com>
2022-11-16 10:36:42 -0500

    nombres mas indicados para las etiquetas

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ export default function Card(props) {
    const { onClose } = props
    return (
       <div id='card'>
-         <button onClick={() => onClose()} id='unob'>X</button>
+         <button onClick={() => onClose()} id='boton1'>X</button>
          <h2>{props.name}</h2>
          <h2>{props.species}</h2>
          <h2>{props.gender}</h2>
