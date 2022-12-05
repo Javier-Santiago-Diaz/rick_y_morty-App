14a1ec3d01580a5cf10604fa4b2866e0e9a7b8bb
javier diaz <santiagodiaz0201@gmail.com>
2022-12-05 11:56:02 -0500

    validation

--- src/components/form/validation.js
+++ src/components/form/validation.js
@@ -1,21 +1,22 @@
-export default function validation(inputs) {
+export default function validation(userDta) {
     const error = {}
 
     const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
-    const regexpass = new RegExp(/^[a-zA-Z0-9!@#$%^&*]{6,16}$/)
+    const regexpass = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/
 
-    if (!regexEmail.test(inputs.username)) {
+    if (!regexEmail.test(userDta.username)) {
         error.username = "Debe ser un correo electronico "
     }
-    if (!inputs.username) {
-        error.username = 'add your name'
+    else if (!userDta.username) {
+        error.username = 'El usurario no puede estar vacio'
     }
-    if (inputs.username.length > 35) {
-        error.username = 'Max 35 caracteres'
+    else if (userDta.username.length > 35) {
+        error.username = 'El usuario no puede tener mas 35 caracteres'
     }
-    if (!regexpass.test(inputs.password)) {
+    else if (!regexpass.test(userDta.password)) {
         error.password = 'No valido minimo entre 6-10 caracteres'
     }
+    else if (userDta.password.length < 6 && userDta.password.length > 10) error.password = 'La contraseña debe ser mayor a 6 y menor a 10 caracteres'
     return error
 }
