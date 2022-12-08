0fad51113cf29a4d3f55f7c00a2b6d9140a6b48f
javier diaz <santiagodiaz0201@gmail.com>
2022-12-07 20:35:54 -0500

    solucionando integration react redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,25 @@
+import { DELETE_FAVORITES, ADD_FAVORITES } from "./types";
+
+const initialState = {
+    myFavorites: []
+}
+
+export default function rootReducer(state = initialState, { types, payload }) {
+    switch (types) {
+        case ADD_FAVORITES:
+            return {
+                ...state,
+                myFavorites: [...state.myFavorites, payload]
+            }
+
+        case DELETE_FAVORITES:
+            const filter = state.myFavorites.filter(ele => ele.id !== payload)
+            return {
+                ...state,
+                myFavorites: filter
+            }
+
+        default:
+            return { ...state }
+    }
+}
