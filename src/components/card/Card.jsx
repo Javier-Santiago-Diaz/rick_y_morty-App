a749b4162169f527b7668407a9765c8ea7afbb50
javier diaz <santiagodiaz0201@gmail.com>
2022-12-08 12:16:50 -0500

    cambios mejores

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,39 @@
-import s from './Card.module.css'
-import { NavLink } from 'react-router-dom'
+import s from './Card.module.css';
+import { NavLink } from 'react-router-dom';
+import { addFavorites, deleteFavorites } from '../../redux/actions';
+import { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
-export default function Card(props) {
-   console.log(props);
+export function Card(props) {
+   const [isFav, setIsfav] = useState(false)
+
+   useEffect(() => {
+      props.myFavorites.forEach((fav) => {
+         if (fav.id === props.id) {
+            setIsfav(true);
+         }
+      });
+   }, [props.myFavorites]);
+
+   function handleFavorite() {
+      if (isFav) {
+         setIsfav(false)
+         props.deleteFavorites(props.id)
+      } else {
+         setIsfav(true)
+         props.addFavorites(props.id)
+      }
+   }
+
    return (
       <div className={s.container}>
+         {
+            isFav ? (
+               <button onClick={handleFavorite}>❤️</button>
+            ) : (
+               <button onClick={handleFavorite}>🤍</button>
+            )
+         }
 
          <div className={s.name}>
             <NavLink to={`/detail/${props.id}`} className={s.link}>
@@ -29,3 +58,22 @@ export default function Card(props) {
       </div >
    );
 }
+
+export function mapDispatchToProps(dispatch) {
+   return {
+      addFavorites: (id) => {
+         dispatch(addFavorites(id))
+      },
+      deleteFavorites: (id) => {
+         dispatch(deleteFavorites(id))
+      }
+   }
+}
+
+export function mapStatetoProps(state) {
+   return {
+      myFavorite: state.myFavorite
+   }
+}
+
+export default connect(mapStatetoProps, mapDispatchToProps)(Card);
