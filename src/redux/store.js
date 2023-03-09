579cc9f8350d1bd46e0419e8194243ee64fdaf1e
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2023-03-08 23:29:49 -0500

    store update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducer';
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
+import {
+    legacy_createStore as createStore,
+    compose,
+    applyMiddleware,
+  } from "redux";
+  import thunk from "redux-thunk";
+  import rootReducer from "./reducer";
+  
+  const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  
+  const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+  
+  export default store;
